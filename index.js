513f1258d5c32494e3463311f6e06c27e0534526
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-04-12 17:11:00 +0200

    feat: created db folder with db.js file. installed dotenv dependency and added require dotenv to index.js. added PORT instance to index.js and app.listen to run port

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express');
 
 const app = express();
 
-
-
-
 const router = require('./router'); 
-
+require('dotenv').config()
 
 app.use(express.json());
 
 app.use(router);
+
+const PORT = process.env.PORT || 4000;
+
+db.then(() => {
+    app.listen(PORT, () => console.log("Server running on port " + PORT));
+  })
+    .catch((err) => console.log(err.message)); 
