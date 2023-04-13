84508414080662a8a9e039f8b37302a868926cbe
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-04-13 20:47:41 +0200

    fix: seeders name in the await was wrong

--- seeders/20230413155900-Artwork.js
+++ seeders/20230413155900-Artwork.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-      await queryInterface.bulkInsert('Roles', [
+      await queryInterface.bulkInsert('Artworks', [
         {id: 1,artist_id:1,title: "100 self-portraits",category:"Digital + paint",description:"A series of self-portraits made by different techniques by hand and digital postproduction",technique:"acrylics + digital",dimensions:"500x500mm",date_creation:"2023-03-01",status:true,image_url:"url.com",price:100,createdAt: "2023-03-01 00:00:00", updatedAt: "2023-03-01 00:00:00"},
         {id: 2,artist_id:1,title: "Blue landscape",category:"Digital + paint",description:"A series of self-portraits made by different techniques by hand and digital postproduction",technique:"acrylics + digital",dimensions:"500x500mm",date_creation:"2023-03-01",status:true,image_url:"url.com",price:100,createdAt: "2023-03-01 00:00:00", updatedAt: "2023-03-01 00:00:00"},
         {id: 3,artist_id:1,title: "Don't look back",category:"Digital + paint",description:"A series of self-portraits made by different techniques by hand and digital postproduction",technique:"acrylics + digital",dimensions:"500x500mm",date_creation:"2023-03-01",status:true,image_url:"url.com",price:100,createdAt: "2023-03-01 00:00:00", updatedAt: "2023-03-01 00:00:00"},
