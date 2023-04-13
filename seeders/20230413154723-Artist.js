fa04cf9770758a19ccdc0426e6afe5d879a1aa3d
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-04-13 20:43:18 +0200

    fix: added missing user_id field to Artist Seeder

--- seeders/20230413154723-Artist.js
+++ seeders/20230413154723-Artist.js
@@ -4,9 +4,9 @@
 module.exports = {
   
     async up (queryInterface, Sequelize) {
-      await queryInterface.bulkInsert('Roles', [
-        {id: 1,artistic_name: "Alex artlab", about_me:"Spanish architect who has passion for painting.",personal_web:"www.mywebart.com",social_media_links:"www.myinstagram.com",createdAt: "2023-03-01 00:00:00", updatedAt: "2023-03-01 00:00:00"},
-        {id: 2,name: "J and Colours", about_me:"Early 30s. I got my degree in arts.",personal_web:"www.myportfolioart.com",social_media_links:"www.myfacebook.com", createdAt: "2023-03-01 00:00:00", updatedAt: "2023-03-01 00:00:00"},
+      await queryInterface.bulkInsert('Artists', [
+        {id: 1,user_id:3,artistic_name: "Alex artlab", about_me:"Spanish architect who has passion for painting.",personal_web:"www.mywebart.com",social_media_links:"www.myinstagram.com",createdAt: "2023-03-01 00:00:00", updatedAt: "2023-03-01 00:00:00"},
+        {id: 2,user_id:5,artistic_name: "J and Colours", about_me:"Early 30s. I got my degree in arts.",personal_web:"www.myportfolioart.com",social_media_links:"www.myfacebook.com", createdAt: "2023-03-01 00:00:00", updatedAt: "2023-03-01 00:00:00"},
         
       ],{});
   },
