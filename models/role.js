8e84a94f8cd5cc8e5274144ee896d88c4d87e902
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-04-13 12:51:28 +0200

    feat: added association model role with model user through foreignKey: role_id

--- models/role.js
+++ models/role.js
@@ -10,7 +10,12 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Role.belongsTo(
+        models.User,
+        {
+          foreignKey: 'role_id'
+        }
+      )
     }
   }
   Role.init({
