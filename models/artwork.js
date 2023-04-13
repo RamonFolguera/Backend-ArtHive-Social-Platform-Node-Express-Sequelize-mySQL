ad0043697117d5ce2605ba2ea20297caece8c575
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-04-13 13:12:04 +0200

    feat: added associations for model artwork with model artists and users through user_artwork

--- models/artwork.js
+++ models/artwork.js
@@ -10,7 +10,16 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      artwork.belongsToMany(
+        models.User,
+        {
+          through: 'User_artworks',
+          foreignKey: 'artwork_id'
+        }
+      ),
+      artwork.belongsTo(
+        models.Artist,
+      )
     }
   }
   artwork.init({
