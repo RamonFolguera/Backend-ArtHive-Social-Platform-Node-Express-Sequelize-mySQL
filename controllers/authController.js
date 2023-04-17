4a412dd0e1d7a6791a08151b1767efac6a3112af
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-04-17 13:52:44 +0200

    feat: added JWT_SECRET work to .end and .env.example. installed jwt token. added login function to authcontroller and its corresponding route.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const { User } = require('../models');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const authController = {};
 
@@ -33,6 +34,52 @@ authController.register = async(req, res) => {
                 message: "Somenthing went wrong with Register",
                 error: error.message
             })
+    }
+}
+
+authController.login = async (req,res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne(
+            {
+                where: {
+                    email:email,
+                }
+            },
+        );
+
+        if(!user) {
+            return res.send("The email address or password is incorrect. Please try again.") 
+        }    
+
+        const passwordMatched = bcrypt.compareSync(password, user.password);
+
+        if(!passwordMatched) {
+            return res.send("The email address or password is incorrect. Please try again.") 
+        }
+
+        const token = jwt.sign(
+            {
+                name: user.name,
+                email: user.email,
+                userId: user.id,
+                roleId: user.role_id
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: '2h' }
+        );
+        return res.json({
+            success: true,
+            message: "Login succesful",
+            data: token
+        })
+        } catch (error) {
+            return res.status(500).json({
+                            success: false,
+                            message: "Somenthing went wrong with Login",
+                            error: error.message
+                        })
     }
 }
 
--- views/authRoutes.js
+++ views/authRoutes.js
@@ -5,6 +5,7 @@ const router = require('express').Router();
 
 
 router.post('/register', authController.register);
+router.post('/login', authController.login);
 
 
 module.exports = router;
