e1e16e51532ccfc65f71e051b529342678956cc9
Ramón Folguera <53578007+brannell@users.noreply.github.com>
2023-04-20 22:58:47 +0200

    feat: installed multer dependency and added code to index.js

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const cors = require('cors')
-
+const path = require("path");
 const db = require('./db/db.js');
 require('dotenv').config()
 
 const app = express();
+const multer = require("multer");
 
 const router = require('./router'); 
+
+const checkFileType = function (file, cb) {
+      //Allowed file extensions
+      const fileTypes = /jpeg|jpg|png|gif|svg/;
+    
+      //check extension names
+      const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+    
+      const mimeType = fileTypes.test(file.mimetype);
+    
+      if (mimeType && extName) {
+        return cb(null, true);
+      } else {
+        cb("Error: You can Only Upload Images!!");
+      }
+    };
 
 let corsOptions = {
     origin: "*",
@@ -17,7 +34,42 @@ let corsOptions = {
     optionsSuccessStatus: 204
 };
 
+//Setting storage engine
+const storageEngine = multer.diskStorage({
+      destination: "./images",
+      filename: (req, file, cb) => {
+        cb(null, `${Date.now()}--${file.originalname}`);
+      },
+    });
+
+    //initializing multer
+    const upload = multer({
+          storage: storageEngine,
+          limits: { fileSize: 10000000 },
+          fileFilter: (req, file, cb) => {
+            checkFileType(file, cb);
+          },
+        });
+
+      
+
 app.use(express.json());
+
+app.post("/single", upload.single("image"), (req, res) => {
+      if (req.file) {
+        res.send("Single file uploaded successfully");
+      } else {
+        res.status(400).send("Please upload a valid image");
+      }
+    });
+
+    app.post("/multiple", upload.array("images", 5), (req, res) => {
+          if (req.files) {
+            res.send("Muliple files uploaded successfully");
+          } else {
+            res.status(400).send("Please upload a valid images");
+          }
+        });
 
 app.use(cors(corsOptions))
 
